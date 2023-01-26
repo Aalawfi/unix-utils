9bda0ca8a6eb95040482088bc51cbf3a1099f0ac
Aalawfi <asalawfi@outlook.com>
2023-01-26 12:06:55 -0500

    Added wgrep utility with initial code

---
+++ wgrep-util/wgrep.c
@@ -0,0 +1,96 @@
+#include <stdio.h> 
+/**
+ *TODO: Description
+ **/
+
+
+// Given a base address for a string, find its length
+size_t findWordLength(char * aWord){ 	
+	size_t count = 0;
+	
+	for(size_t i = 0; aWord[i] != '\0'; i++){
+		count++;
+	}
+
+	return count;
+}
+
+// given a : line, its length, keyword, its length
+//  return 1 or 0 if a keyword exists in that line 
+int checkForKeyword(char * line, size_t lineLength, 
+		    char * keyword, size_t keywordLength){
+	
+	size_t i, j;
+
+	for (i = 0; i < lineLength - keywordLength; i++) {
+		for (j = i; j < i + keywordLength; j++) {
+			
+			if (line[j] != keyword[j - i]){
+				break;
+			}
+		  }
+		if (j == i + keywordLength){
+			return 1; 
+		}
+	}
+        return 0;
+
+}
+// Opens a file, read it line by line, and print out the line if a given keyword
+// exists in it
+int searchInFile(FILE * searchFile, char * keyword, size_t keywordLength){
+
+	if(searchFile == NULL){
+		printf("Couldn't find file \n");
+		return -1;
+	}
+
+	char * lineptr = NULL; 
+	size_t n = 0;
+	ssize_t readChars; 
+	while((readChars = getline(&lineptr, &n, searchFile)) != -1){
+
+		// find length of the line 
+		size_t lineLength = findWordLength(lineptr);
+
+		// print out the line if the keyword exists
+		if(checkForKeyword(lineptr, lineLength, keyword, keywordLength) == 1){
+			printf("%s\n", lineptr);
+		}
+ 
+	} 
+	return 0;
+}
+
+
+int main(int argc, char *argv[]){
+
+	// No input at all
+	if(argc <= 1){
+		printf("No input was given\n");
+		return -1;
+	}
+
+	size_t keywordLength = findWordLength(argv[1]);
+	
+	// search term given but no files, 
+	if(argc == 2){
+	
+	char userInput[40];
+	fscanf(stdin, "%s", userInput);
+
+	// Apply searchInFile on stidn file
+	searchInFile(stdin, argv[1], keywordLength);
+	fclose(stdin);
+	return 0;	
+	}
+
+	
+	// This works on single file, 
+	// figure out how to make it work on multiple files
+	FILE * searchFile = fopen(argv[2], "r");
+	searchInFile(searchFile, argv[1], keywordLength);
+	fclose(searchFile);
+	return 0;
+
+}
